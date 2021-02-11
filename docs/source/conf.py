62cd6dadf8bb500434dc0da65cbe5caffbf46b37
raproid <raproid@gmail.com>
2021-02-12 02:53:55 +0300

    test locally added furo attemps 3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,7 +70,7 @@ html_extra_path = ['_public']
 
 html_theme = "furo"
 
-html_theme_path = ["_themes/furo"]
+html_theme_path = ['./furo.zip']
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
