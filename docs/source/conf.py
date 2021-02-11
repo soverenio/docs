33748209364f1fd0dad13b74320375fcef8aed40
raproid <raproid@gmail.com>
2021-02-12 02:46:21 +0300

    test locally added furo attemps 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,9 +67,11 @@ html_extra_path = ['_public']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+
 html_theme = "furo"
 
-html_theme_path = ["furo"]
+html_theme_path = ["_themes/furo"]
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
