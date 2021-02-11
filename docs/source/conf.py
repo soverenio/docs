75e2277b10e8292467cfab143b2ca276e7cb7598
raproid <raproid@gmail.com>
2021-02-12 02:27:44 +0300

    readd furo to  conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,8 @@ extensions = [
 	'sphinx_tabs.tabs',
 	'sphinxcontrib.plantuml',
 	'sphinx_copybutton',
-    'sphinx_panels'
+    'sphinx_panels',
+    'furo'
 #   'hoverxref.extension'
 #	'sphinxcontrib.contentui'
 ]
