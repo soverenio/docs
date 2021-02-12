1553aa2d95ee574bf7a9a6fceee5d5d96efa61c5
raproid <raproid@gmail.com>
2021-02-12 14:38:59 +0300

    create dpo portal registration instruction

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,8 +49,8 @@ master_doc = 'index'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-#def setup(app):
-#    app.add_css_file('css/custom_furo.css')
+def setup(app):
+    app.add_css_file('css/custom_furo.css')
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
